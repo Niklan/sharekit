9714fd32846c4556e68dc2da25d4d419150916c7
Niklan <niklanrus@gmail.com>
2015-01-26 07:14:02 -0500

    Added simple style. Several fixes and improvements.

--- theme/sharek.tpl.php
+++ theme/sharek.tpl.php
@@ -6,11 +6,30 @@
  * Variables:
  * - $network
  * - $share_url
+ * - $share_title
  * - $label
  * - $popup_width
  * - $popup_height
+ * - $attributes: print default attributes. If you don't want to print them,
+ *                you must add them manually.
+ * - $classes
+ * - $share_count
+ *
+ * Template suggestions:
+ * - sharek--NETWORK--STYLE.tpl.php
+ * - sharek--STYLE.tpl.php
+ * - sharek--NETWORK.tpl.php
+ * - sharek.tpl.php
+ *
+ * For example. Button called buy sharek_get_ubtton('facebook', 'flat) can be
+ * themed by templates:
+ * - sharek--facebook--flat.tpl.php
+ * - sharek--flat.tpl.php
+ * - sharek--facebook.tpl.php
+ * - sharek.tpl.php
  */
 ?>
-<button data-share-url="<?php print $share_url; ?>" data-popup-width="<?php print $popup_width; ?>" data-popup-height="<?php print $popup_height; ?>">
-  <?php print $label; ?>
-</button>
+<div class="<?php print $classes; ?>" <?php print $attributes; ?>>
+  <span class="share-label"><?php print $label; ?></span>
+  <span class="share-count"><?php print $share_count; ?></span>
+</div>
